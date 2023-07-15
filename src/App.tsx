db8504a24480d3ac68856af2efa2280d17760ad1
MariamKvirchiladze <kvircho555@gmail.com>
2023-07-16 01:59:59 +0400

    create Balance component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
+import styled from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+import Balance from "./components/Balance";
 function App() {
-  return <></>;
+  return (
+    <MainContainer>
+      <GlobalStyles />
+      <Balance />
+    </MainContainer>
+  );
 }
 
 export default App;
+
+const MainContainer = styled.div`
+  width: 100%;
+  min-height: 100vh;
+  background-color: #f8e9dd;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 16px;
+
+  @media (min-width: 1024px) {
+    gap: 24px;
+  }
+`;
---
+++ src/components/Balance.tsx
@@ -0,0 +1,58 @@
+import logo from "../assets/logo.svg";
+import styled from "styled-components";
+
+const Balance = (): JSX.Element => {
+  return (
+    <BalanceWrapper>
+      <div>
+        <h4>My balance</h4>
+        <h2>$921.48</h2>
+      </div>
+      <img src={logo} />
+    </BalanceWrapper>
+  );
+};
+
+export default Balance;
+
+const BalanceWrapper = styled.div`
+  width: 343px;
+  background-color: #ec755d;
+  border-radius: 10px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 20px;
+  @media (min-width: 1024px) {
+    width: 540px;
+    padding: 27px 39px 25px 32px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    gap: 4px;
+
+    h4 {
+      font-size: 15px;
+      font-weight: 400;
+      color: white;
+      line-height: 19.5px;
+      @media (min-width: 1024px) {
+        font-size: 18px;
+        line-height: 23.44px;
+      }
+    }
+
+    h2 {
+      font-size: 24px;
+      font-weight: 700;
+      color: white;
+      line-height: 31.25px;
+      @media (min-width: 1024px) {
+        font-size: 32px;
+        line-height: 41.66px;
+      }
+    }
+  }
+`;
